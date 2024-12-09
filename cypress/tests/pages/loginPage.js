604d12c012455d401c904a20ab72196153de68cd
eainathh <nathaliagsm02@gmail.com>
2024-12-09 07:44:46 -0300

    primeiras configurações

---
+++ cypress/tests/pages/loginPage.js
@@ -0,0 +1,31 @@
+class LoginPage {
+    selectorList() {
+        const selectors = {
+            usernameField: "[name='username']",
+            passwordField: "[name='password']",
+            loginButton: ".SignInForm-submit",
+            wrongCredentialsAlert: "[data-test='signin-error']",
+        }
+
+        return selectors;
+
+    }
+
+    accessLoginPage() {
+        cy.visit('http://localhost:3001/signin')
+    }
+
+    loginWithUser(username, password) {
+        cy.get(this.selectorList().usernameField).type(username)
+        cy.get(this.passwordField().passwordField).type(password)
+    }
+
+    buttonLogin() {
+        cy.get(this.selectorList().loginButton).click()
+    }
+
+    alertWrongCredentials() {
+        cy.get(this.selectorList().loginButton).click()
+    }
+
+}
---
+++ cypress/tests/user/login.spec.cy.js
@@ -0,0 +1,5 @@
+describe('Login com sucesso', () => {
+    it.only('Deve fazer login com um usuário válido', () => {
+      
+    });
+  });
